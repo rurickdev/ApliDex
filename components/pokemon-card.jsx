088ff9e1ae79fe6e2cf83494f06e20a3a5162e6d
Rurick Maqueo <skintigthvader@gmail.com>
2020-01-22 22:02:34 -0600

    initial structure for the card

--- components/pokemon-card.jsx
+++ components/pokemon-card.jsx
@@ -1,7 +1,27 @@
 
+import TypeIcon from '../components/type-icon'
+
 function PokemonCard (props) {
+  const { pokemon } = props
+
+  // ToDo: redo the structure
   return (
-    <h1>Soy una card</h1>
+    <div className='is-pokemon-card card'>
+      <figure className='is-pokemon-image-container card-image'>
+        <img className='is-pokemon-image' src={pokemon.sprites.front_default} alt={pokemon.name} />
+        <span className='is-pokemon-number'>#{pokemon.id}</span>
+        <div className='is-pokemon-type-container columns is-marginless is-paddingless is-mobile'>
+          {pokemon.types.map((type, index) => (
+            <TypeIcon type={type.type.name} key={index} />
+          ))}
+        </div>
+      </figure>
+      <div className='is-pokemon-data card-content columns is-multiline is-marginless is-paddingless'>
+        <div className='is-pokemon-name column is-full is-size-3'>
+          <span>{pokemon.name}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
