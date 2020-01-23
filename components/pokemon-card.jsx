7eb237cfc6d2211bcc6c817dea443a00f03e364f
Rurick Maqueo <skintigthvader@gmail.com>
2020-01-23 15:10:04 -0600

    First version of the pokemon card

--- components/pokemon-card.jsx
+++ components/pokemon-card.jsx
@@ -1,25 +1,31 @@
 
 import TypeIcon from '../components/type-icon'
 
+const ghostImage = 'http://cdn.aplidex.rurick.dev.s3.us-east-2.amazonaws.com/assets/ghost.png'
+
 function PokemonCard (props) {
   const { pokemon } = props
 
-  // ToDo: redo the structure
   return (
     <div className='is-pokemon-card card'>
+
       <figure className='is-pokemon-image-container card-image'>
-        <img className='is-pokemon-image' src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <span className='is-pokemon-number'>#{pokemon.id}</span>
-        <div className='is-pokemon-type-container columns is-marginless is-paddingless is-mobile'>
-          {pokemon.types.map((type, index) => (
-            <TypeIcon type={type.type.name} key={index} />
-          ))}
-        </div>
+        <img className='is-pokemon-image' src={pokemon.sprites.front_default || ghostImage} alt={pokemon.name} />
       </figure>
-      <div className='is-pokemon-data card-content columns is-multiline is-marginless is-paddingless'>
-        <div className='is-pokemon-name column is-full is-size-3'>
-          <span>{pokemon.name}</span>
-        </div>
+
+      <div className='is-pokemon-data card-content columns is-mobile is-multiline is-marginless is-paddingless is-vcentered'>
+
+        <span className='is-pokemon-name column is-full is-size-3 is-size-4-mobile'>
+          {pokemon.name}
+        </span>
+
+        <span className='is-pokemon-number column'>#{pokemon.id}</span>
+
+        {pokemon.types.map((type, index) => (
+          <div className='column is-one-quarter is-paddingless' key={index}>
+            <TypeIcon type={type.type.name} />
+          </div>
+        ))}
       </div>
     </div>
   )
