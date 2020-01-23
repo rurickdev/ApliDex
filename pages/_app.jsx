edea86ddc86b598a7db130a181a06e00428ffcde
Rurick Maqueo <skintigthvader@gmail.com>
2020-01-23 13:14:16 -0600

    Implement and test the aplidex wrapper

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,51 +1,53 @@
 
-import App from 'next/app'
 import Head from 'next/head'
+
+import pokedex from '../lib/api'
 
 import '../sass/index.scss'
 
-// ToDo: Delete this
-const many = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+function MyApp (props) {
+  const { Component, pokemons } = props
+  console.log('[pageProps]', pokemons.length)
+  return (
+    <>
+      <Head>
+        <title>ApliDex</title>
+      </Head>
+      <div className='columns is-multiline is-marginless is-paddingless'>
+        <aside className='column is-one-quarter is-hidden-mobile'>
+          <div className='details-container'>
+            <h1>Details</h1>
+          </div>
+        </aside>
 
-class MyApp extends App {
-  render () {
-    const { Component } = this.props
-
-    return (
-      <>
-        <Head>
-          <title>ApliDex</title>
-        </Head>
-        <div className='columns is-multiline is-marginless is-paddingless'>
-          <aside className='column is-one-quarter is-hidden-mobile'>
-            <div className='details-container'>
-              <h1>Details</h1>
-            </div>
-          </aside>
-
-          <div className='column is-three-quarters has-background-white-ter'>
-            <div className='columns is-multiline is-mobile is-marginless is-paddingless'>
-              <div className='column is-full'>
-                {/* ToDo: Crear el componente buscador */}
-                <div className='search-bar'>
-                  <h1>Buscador</h1>
-                </div>
+        <div className='column is-three-quarters has-background-white-ter'>
+          <div className='columns is-multiline is-mobile is-marginless is-paddingless'>
+            <div className='column is-full'>
+              {/* ToDo: Crear el componente buscador */}
+              <div className='search-bar'>
+                <h1>Buscador</h1>
               </div>
-              {/* ToDo: Cambiar esto por la petición a pokeapi */}
-              {many.map((element, index) => (
-                <div className='column is-half-mobile is-one-third-tablet is-one-third-desktop' key={index}>
-                  <Component />
-                </div>
-              ))}
             </div>
+            {/* ToDo: Cambiar esto por la petición a pokeapi */}
+            <Component pokemon={pokemons[0]} />
           </div>
         </div>
-        <footer className='details-container'>
-          <h1>Details</h1>
-        </footer>
-      </>
-    )
-  }
+      </div>
+      <footer className='details-container'>
+        <h1>Details</h1>
+      </footer>
+    </>
+  )
+}
+
+MyApp.getInitialProps = async () => {
+  const { results } = await pokedex.getPokemonsList()
+
+  const pokemonListWithDetailsPromises = results.map(pokemon => pokedex.getPokemonByName(pokemon.name))
+
+  const pokemonListWithDetails = await Promise.all(pokemonListWithDetailsPromises)
+
+  return { pokemons: pokemonListWithDetails }
 }
 
 export default MyApp
