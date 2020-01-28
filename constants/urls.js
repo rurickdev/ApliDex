0d99b2aeab1039ed254fa1e6a38d2b2b73261881
Rurick Maqueo <skintigthvader@gmail.com>
2020-01-27 18:53:11 -0600

    used https urls

--- constants/urls.js
+++ constants/urls.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  ghostImage: 'https://s3.us-east-2.amazonaws.com/cdn.aplidex.rurick.dev/assets/no_sprite.png',
+  noSprite: 'https://s3.us-east-2.amazonaws.com/cdn.aplidex.rurick.dev/assets/no_sprite.png',
   apiEndPoint: 'https://fvit92grgf.execute-api.us-east-1.amazonaws.com/default/rurickLambda'
 }
